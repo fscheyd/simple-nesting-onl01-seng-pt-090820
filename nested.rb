ad362f18e7f29b94df0edb88876b11cef8fb88fb
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-19 18:27:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -118,7 +118,7 @@ def changing_alan
 end
 
 def adding_to_dennis
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -133,5 +133,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
